6eaa021cd295e9cca76b75ccecf1d00db8d173ba
Raccoon Ryker <83371915+raccoondev142@users.noreply.github.com>
2024-01-13 09:18:38 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace MineManage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshModsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 46);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(521, 266);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(513, 240);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mods";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(201, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "mods Folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Forge Website";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import a Mod";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 7);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(500, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshModsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(114, 26);
+            // 
+            // refreshModsToolStripMenuItem
+            // 
+            this.refreshModsToolStripMenuItem.Name = "refreshModsToolStripMenuItem";
+            this.refreshModsToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.refreshModsToolStripMenuItem.Text = "Refresh";
+            this.refreshModsToolStripMenuItem.Click += new System.EventHandler(this.refreshModsToolStripMenuItem_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(513, 240);
+            this.tabPage2.TabIndex = 4;
+            this.tabPage2.Text = "Resource Packs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(104, 196);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 38);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "resourcepacks Folder";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 196);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 38);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Import a Resource Pack";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(7, 7);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(500, 186);
+            this.listBox2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button9);
+            this.tabPage3.Controls.Add(this.button7);
+            this.tabPage3.Controls.Add(this.button8);
+            this.tabPage3.Controls.Add(this.listBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(513, 240);
+            this.tabPage3.TabIndex = 5;
+            this.tabPage3.Text = "Worlds";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(201, 196);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(91, 38);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "Edit World Data (coming soon!)";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(104, 196);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 38);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "saves Folder";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(7, 196);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(91, 38);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Import a World";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(7, 7);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(500, 186);
+            this.listBox3.TabIndex = 2;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label2);
+            this.tabPage4.Controls.Add(this.pictureBox1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(513, 240);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "About";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 65);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MineManage\r\nv2.0\r\n\r\nMaintained by TreeDeveloper.\r\nWrench icon from iconarchive.co" +
+    "m";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MineManage.Properties.Resources.minemanage;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Minecraft Path:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 324);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MineManage";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem refreshModsToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
+
+namespace MineManage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "/config.txt"))
+            {
+                string[] fr = File.ReadAllLines(Application.StartupPath + "/config.txt");
+                if (Directory.Exists(fr[1] + "/mods"))
+                {
+                    textBox1.Text = fr[1];
+                    foreach (string name in Directory.GetFiles(textBox1.Text + "/mods"))
+                    {
+                        listBox1.Items.Add(Path.GetFileName(name));
+                    }
+                    foreach (string name in Directory.GetDirectories(textBox1.Text + "/resourcepacks"))
+                    {
+                        listBox2.Items.Add(Path.GetFileName(name));
+                    }
+                    foreach (string name in Directory.GetDirectories(textBox1.Text + "/saves"))
+                    {
+                        listBox3.Items.Add(Path.GetFileName(name));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You don't have a version of forge installed.\nPlease install a version of forge at 'files.minecraftforge.net'.", "Forge not installed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                File.WriteAllText(Application.StartupPath + "/config.txt", "//MineManage Configuration : DO NOT MODIFY!\nC:/Users/" + Environment.UserName + "/AppData/Roaming/.minecraft");
+                MessageBox.Show("Welcome to MineManage!\nThe setup is done, and now the app is yours.\nThe program will now close, and you can open it again to see the features.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog mod = new OpenFileDialog();
+            mod.Title = "Import Mod";
+            mod.Filter = "Java Mods (*.jar)|*.jar";
+            if (mod.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.Move(mod.FileName, textBox1.Text + "/mods/" + Path.GetFileName(mod.FileName));
+                    Reload();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://files.minecraftforge.net");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(textBox1.Text + "/mods/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(Application.StartupPath + "/config.txt", "//MineManage Configuration : DO NOT MODIFY!\n" + fbd.SelectedPath);
+            }
+        }
+
+        private void refreshModsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog mod = new OpenFileDialog();
+            mod.Title = "Import Resource Pack";
+            mod.Filter = "Zipped Folder (*.zip)|*.zip";
+            if (mod.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory(mod.FileName, textBox1.Text + "/resourcepacks/" + Path.GetFileNameWithoutExtension(mod.FileName));
+                    Reload();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Process.Start(textBox1.Text + "/resourcepacks/");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process.Start(textBox1.Text + "/saves/");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog mod = new OpenFileDialog();
+            mod.Title = "Import Minecraft World";
+            mod.Filter = "Zipped Folder (*.zip)|*.zip";
+            if (mod.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory(mod.FileName, textBox1.Text + "/saves/" + Path.GetFileNameWithoutExtension(mod.FileName));
+                    Reload();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Reload()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            foreach (string name in Directory.GetFiles(textBox1.Text + "/mods"))
+            {
+                listBox1.Items.Add(Path.GetFileName(name));
+            }
+            foreach (string name in Directory.GetDirectories(textBox1.Text + "/resourcepacks"))
+            {
+                listBox2.Items.Add(Path.GetFileName(name));
+            }
+        }
+    }
+}
